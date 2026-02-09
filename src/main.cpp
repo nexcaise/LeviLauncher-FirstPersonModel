5375ee2306051bd24e67516682c026e2ef0af69e
alvinqid <github@alvinq.my.id>
2026-02-09 20:48:49 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,125 +1,78 @@
-#include <unistd.h>
-#include <dlfcn.h>
-#include <cmath>
-#include <string>
-
 #include <cstdint>
+#include <dlfcn.h>
 
 #include "pl/Signature.h"
 #include "pl/Hook.h"
-#include "util/Logger.hpp"
-
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <cstdio>
-#include <unistd.h>
-#include <sys/mman.h>
-
 #include "pl/Gloss.h"
 
-bool HOOK(
-    const char* sig,
-    void* hook,
-    void** orig
-) {
-    uintptr_t addr = pl::signature::pl_resolve_signature(sig, "libminecraftpe.so");
-    if (!addr) {
-        return false;
-    }
+constexpr uintptr_t CAMERA_POS_OFFSET = 0x18;
 
-    int ret = pl::hook::pl_hook((pl::hook::FuncPtr)addr, (pl::hook::FuncPtr)hook, (pl::hook::FuncPtr*)&orig, pl::hook::PriorityHighest);
-    if (ret != 0) {
-        return false;
-    }
-
-    return true;
-}
-
-// Camera offset values
 static float g_CameraOffsetX = 0.0f;
 static float g_CameraOffsetY = -0.2f;
 static float g_CameraOffsetZ = 0.5f;
-static float g_ModelScale = 1.0f;
 
-// Function signatures
-//static void* (*CameraGetPosition)(void* camera, float* x, float* y, float* z) = nullptr;
-//static void* (*CameraSetPosition)(void* camera, float x, float y, float z) = nullptr;
-//static void* (*CameraGetRotation)(void* camera, float* pitch, float* yaw) = nullptr;
-//static void* (*CameraSetRotation)(void* camera, float pitch, float yaw) = nullptr;
-//static void* (*RenderPlayerModel)(void* player, float deltaTime) = nullptr;
-
-void CameraSetPosition(void* camera, float x, float y, float z) {
-    if (!camera) {
-        return;
-    }
-    
-    // Read current position
-    size_t posOffset = 0x18;
-    float* posX = reinterpret_cast<float*>(reinterpret_cast<uintptr_t>(camera) + posOffset);
-    float* posY = reinterpret_cast<float*>(reinterpret_cast<uintptr_t>(camera) + posOffset + sizeof(float));
-    float* posZ = reinterpret_cast<float*>(reinterpret_cast<uintptr_t>(camera) + posOffset + 2 * sizeof(float));
-    
-    *posX = x;
-    *posY = y;
-    *posZ = z;
+static inline float* CameraPos(void* camera) {
+    return reinterpret_cast<float*>(
+        reinterpret_cast<uintptr_t>(camera) + CAMERA_POS_OFFSET
+    );
 }
 
-static void* (*orig_CameraRender)(void* camera, float deltaTime) = nullptr;
+static inline void CameraSetPosition(void* camera, float x, float y, float z) {
+    if (!camera) return;
+    float* pos = CameraPos(camera);
+    pos[0] = x;
+    pos[1] = y;
+    pos[2] = z;
+}
+
+static void* (*orig_CameraRender)(void*, float) = nullptr;
+
 static void* hook_CameraRender(void* camera, float deltaTime) {
-    //if (g_FirstPersonModelEnabled && RenderPlayerModel) {
-        // Get current camera position and rotation
-        float camX, camY, camZ;
-        float pitch, yaw;
-        
-        //if (CameraGetPosition) {
-            //CameraGetPosition(camera, &camX, &camY, &camZ);
-            // Adjust camera position for first person model view
-            CameraSetPosition(camera, camX + g_CameraOffsetX, camY + g_CameraOffsetY, camZ + g_CameraOffsetZ);
-        //}
-        
-        // Render player model
-        /*void* player = nullptr; // Get local player instance
-        if (player) {
-            RenderPlayerModel(player, deltaTime);
-        }*/
-        
-        // Restore original camera position
-        //if (CameraGetPosition) {
-            CameraSetPosition(camera, camX, camY, camZ);
-        //}
-    //}
-    
-    return orig_CameraRender ? orig_CameraRender(camera, deltaTime) : nullptr;
+    if (camera) {
+        float* pos = CameraPos(camera);
+
+        float ox = pos[0];
+        float oy = pos[1];
+        float oz = pos[2];
+
+        pos[0] = ox + g_CameraOffsetX;
+        pos[1] = oy + g_CameraOffsetY;
+        pos[2] = oz + g_CameraOffsetZ;
+
+        void* ret = orig_CameraRender
+            ? orig_CameraRender(camera, deltaTime)
+            : nullptr;
+
+        pos[0] = ox;
+        pos[1] = oy;
+        pos[2] = oz;
+
+        return ret;
+    }
+
+    return orig_CameraRender
+        ? orig_CameraRender(camera, deltaTime)
+        : nullptr;
 }
 
-/*
-static void InitCameraAPI() {
-    void* mcpe = dlopen("libminecraftpe.so", RTLD_LAZY);
-    if (!mcpe) return;
-    
-    CameraGetPosition = (void* (*)(void*, float*, float*, float*))dlsym(mcpe, 
-        "_ZN6Camera11getPositionEPfS0_S0_");
-    CameraSetPosition = (void* (*)(void*, float, float, float))dlsym(mcpe,
-        "_ZN6Camera11setPositionEfff");
-    CameraGetRotation = (void* (*)(void*, float*, float*))dlsym(mcpe,
-        "_ZN6Camera11getRotationEPfS0_");
-    CameraSetRotation = (void* (*)(void*, float, float))dlsym(mcpe,
-        "_ZN6Camera11setRotationEff");
-    
-    // Player model render function
-    RenderPlayerModel = (void* (*)(void*, float))dlsym(mcpe,
-        "_ZN6Player11renderModelEf");
-}
-*/
+static inline bool Hook(const char* sig, void* hook, void** orig) {
+    uintptr_t addr = pl::signature::pl_resolve_signature(sig, "libminecraftpe.so");
+    if (!addr) return false;
 
-static void HookCamera() {
-    HOOK("48 ?? ?? ?? 21 ?? ?? 91 ?? ?? 00 ?? ?? 91 ?? ?? 40 ?? ?? 00 00 ?? ?? 00 94", (void*)hook_CameraRender, (void**)&orig_CameraRender);
+    return pl::hook::pl_hook(
+        (pl::hook::FuncPtr)addr,
+        (pl::hook::FuncPtr)hook,
+        (pl::hook::FuncPtr*)orig,
+        pl::hook::PriorityHighest
+    ) == 0;
 }
 
 __attribute__((constructor))
-void FirstPersonModel_Init() {
+static void FirstPersonModel_Init() {
     GlossInit(true);
-    //InitCameraAPI();
-    HookCamera();
+    Hook(
+        "48 ?? ?? ?? 21 ?? ?? 91 ?? ?? 00 ?? ?? 91 ?? ?? 40 ?? ?? 00 00 ?? ?? 00 94",
+        (void*)hook_CameraRender,
+        (void**)&orig_CameraRender
+    );
 }
